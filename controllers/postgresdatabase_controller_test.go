273eab0f222e14c807148e54cf956be915f89042
Bujar Murati <39311213+BujarMurati@users.noreply.github.com>
2021-11-06 21:45:31 +0100

    [BM] refactor test setup

--- controllers/postgresdatabase_controller_test.go
+++ controllers/postgresdatabase_controller_test.go
@@ -16,6 +16,13 @@ import (
 	dbv1beta1 "github.com/bujarmurati/pg-db-operator/api/v1beta1"
 )
 
+const (
+	PostgresDatabaseName = "db"
+	timeout              = time.Second * 10
+	duration             = time.Second * 10
+	interval             = time.Millisecond * 250
+)
+
 func assertDatumInSecret(key, value string, data map[string][]byte) {
 	Expect(data).Should(HaveKey(key))
 	actualValue, err := b64decode(data[key])
@@ -23,17 +30,49 @@ func assertDatumInSecret(key, value string, data map[string][]byte) {
 	Expect(actualValue).To(Equal(value))
 }
 
+var DefaultPostgresDatabase = &dbv1beta1.PostgresDatabase{
+	TypeMeta: metav1.TypeMeta{
+		APIVersion: "db.bujarmurati.com/v1beta1",
+		Kind:       "PostgresDatabase",
+	},
+	ObjectMeta: metav1.ObjectMeta{
+		Name: PostgresDatabaseName,
+	},
+}
+
+func defaultPostgresDatabase(namespace string) *dbv1beta1.PostgresDatabase {
+	return &dbv1beta1.PostgresDatabase{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "db.bujarmurati.com/v1beta1",
+			Kind:       "PostgresDatabase",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      PostgresDatabaseName,
+			Namespace: namespace,
+		},
+		Spec: dbv1beta1.PostgresDatabaseSpec{
+			DatabaseName:    "db-" + namespace,
+			SecretName:      "secret-" + namespace,
+			UserNamePostFix: "@" + connConfig.Host,
+		},
+	}
+}
+
+func waitForPostgresDatabaseResourceCreation(ctx context.Context, namespace string, pgdb *dbv1beta1.PostgresDatabase) *dbv1beta1.PostgresDatabase {
+
+	Expect(k8sClient.Create(ctx, pgdb)).Should(Succeed())
+	pgdbLookupKey := types.NamespacedName{Name: pgdb.Name, Namespace: namespace}
+	createdPostgresDatabase := &dbv1beta1.PostgresDatabase{}
+
+	Eventually(func() bool {
+		err := k8sClient.Get(ctx, pgdbLookupKey, createdPostgresDatabase)
+		return err == nil
+	}, timeout, interval).Should(BeTrue())
+	return createdPostgresDatabase
+}
+
 var _ = Describe("PostgresDatabase controller", func() {
 
-	// Define utility constants for object names and testing timeouts/durations and intervals.
-	const (
-		PostgresDatabaseName = "db"
-		DatabaseName         = "db"
-		SecretName           = "secret-db"
-		timeout              = time.Second * 10
-		duration             = time.Second * 10
-		interval             = time.Millisecond * 250
-	)
 	kind := reflect.TypeOf(dbv1beta1.PostgresDatabase{}).Name()
 	gvk := dbv1beta1.GroupVersion.WithKind(kind)
 	var PostgresDatabaseNamespace string
@@ -58,29 +97,9 @@ var _ = Describe("PostgresDatabase controller", func() {
 		Context("and no matching secret exists", func() {
 			It("Should create a new secret", func() {
 				ctx := context.Background()
-				postgresDatabase := &dbv1beta1.PostgresDatabase{
-					TypeMeta: metav1.TypeMeta{
-						APIVersion: "db.bujarmurati.com/v1beta1",
-						Kind:       "PostgresDatabase",
-					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      PostgresDatabaseName,
-						Namespace: PostgresDatabaseNamespace,
-					},
-					Spec: dbv1beta1.PostgresDatabaseSpec{
-						DatabaseName:    "db-" + PostgresDatabaseNamespace,
-						SecretName:      "secret-" + PostgresDatabaseNamespace,
-						UserNamePostFix: "@" + connConfig.Host,
-					},
-				}
-				Expect(k8sClient.Create(ctx, postgresDatabase)).Should(Succeed())
-				pgdbLookupKey := types.NamespacedName{Name: PostgresDatabaseName, Namespace: PostgresDatabaseNamespace}
-				createdPostgresDatabase := &dbv1beta1.PostgresDatabase{}
-
-				Eventually(func() bool {
-					err := k8sClient.Get(ctx, pgdbLookupKey, createdPostgresDatabase)
-					return err == nil
-				}, timeout, interval).Should(BeTrue())
+				postgresDatabase := defaultPostgresDatabase(PostgresDatabaseNamespace)
+				postgresDatabase.Spec.UserNamePostFix = "@" + connConfig.Host
+				createdPostgresDatabase := waitForPostgresDatabaseResourceCreation(ctx, PostgresDatabaseNamespace, postgresDatabase)
 				By("Creating a new opaque secret")
 				var createdSecret *corev1.Secret
 				Eventually(func() (err error) {
